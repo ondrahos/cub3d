615dfcd360d1dc8a25b523cf2e322c94d538be88
Dimitri Araz <dimitri.araz@gmail.com>
2024-05-04 17:18:01 +0200

    add first check for argument and invalid map

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: daraz <daraz@student.42prague.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 17:53:12 by ohosnedl          #+#    #+#             */
-/*   Updated: 2024/05/04 17:12:27 by daraz            ###   ########.fr       */
+/*   Updated: 2024/05/04 17:17:51 by daraz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,6 @@ static int	parse_argument(t_game *game, char **av)
 {
 	if (check_file(av[1], true) == FAIL)
 		close_exit(game, FAIL);
-	/* parse_data(av[1], game);
-	if (get_file_data(data, data->mapinfo.file) == FAIL)
-		return (free_data(data));
-	if (check_map_validity(data, data->map) == FAIL)
-		return (free_data(data));
-	if (check_textures_validity(data, &data->texinfo) == FAIL)
-		return (free_data(data)); */
-	/* init_player_direction(data); */
-	/* if (DEBUG_MSG)
-		debug_display_data(data); */
 	return (0);
 }
 
